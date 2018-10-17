23cff7319742bdf8e7a23413c0244dbdf3c0e2be
Alopex6414 <1029535012@qq.com>
2018-10-17 16:53:03 +0800

    CerasusControl Add Method.

--- CerasusControl/CerasusGrowArray.h
+++ CerasusControl/CerasusGrowArray.h
@@ -3,7 +3,7 @@
 *     Copyright(c) 2018, Team Shanghai Dream Equinox
 *     All rights reserved.
 *
-* @file		CreasusGrowArray.h
+* @file		CerasusGrowArray.h
 * @brief	This File is CreasusGrowArray Header File.
 * @author	Alopex/Helium
 * @version	v1.00a
@@ -14,8 +14,54 @@
 #ifndef __CERASUSGROWARRAY_H_
 #define __CERASUSGROWARRAY_H_
 
+//Include Direct Common Header File
+#include "DirectCommon.h"
 
+//Class Definition
+template<typename TYPE> class CGrowableArray
+{
+protected:
+	TYPE* m_pData;
+	int m_nSize;
+	int m_nMaxSize;
 
+protected:
+	HRESULT SetSizeInternal(int nNewMaxSize);
 
-#endif // !__CREASUSGROWARRAY_H_
+public:
+	CGrowableArray();
+	CGrowableArray(const CGrowableArray <TYPE>& a);
+	~CGrowableArray();
 
+	const TYPE& operator[](int nIndex) const;
+	TYPE& operator[](int nIndex);
+	CGrowableArray& operator=(const CGrowableArray <TYPE>& a)
+	{
+		if (this == &a) return *this; RemoveAll(); for (int i = 0; i < a.m_nSize;
+			i++) Add(a.m_pData[i]); return *this;
+	}
+
+	HRESULT SetSize(int nNewMaxSize);
+	HRESULT Add(const TYPE& value);
+	HRESULT Insert(int nIndex, const TYPE& value);
+	HRESULT SetAt(int nIndex, const TYPE& value);
+	TYPE& GetAt(int nIndex) const;
+	int GetSize() const;
+	TYPE* GetData();
+	bool Contains(const TYPE& value);
+
+	int IndexOf(const TYPE& value);
+	int IndexOf(const TYPE& value, int iStart);
+	int IndexOf(const TYPE& value, int iStart, int nNumElements);
+
+	int LastIndexOf(const TYPE& value);
+	int LastIndexOf(const TYPE& value, int nIndex);
+	int LastIndexOf(const TYPE& value, int iEnd, int nNumElements);
+
+	HRESULT Remove(int nIndex);
+	void RemoveAll();
+	void Reset();
+
+};
+
+#endif // !__CERASUSGROWARRAY_H_

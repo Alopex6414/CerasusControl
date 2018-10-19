6f007ef42076736b8b809ff3061447f7169c6fca
Alopex6414 <1029535012@qq.com>
2018-10-19 16:56:46 +0800

    CerasusControl Add Method.

--- CerasusControl/CerasusBlend.cpp
+++ CerasusControl/CerasusBlend.cpp
@@ -25,6 +25,8 @@ CCerasusBlendTex::CCerasusBlendTex(IDirect3DDevice9* pD3D9Device)
 	for (int i = 0; i < MAX_CONTROL_STATES; ++i)
 	{
 		m_pTexStates[i] = new CCerasusUnit(pD3D9Device);
+		memset(&m_sUnitArr[i], 0, sizeof(CUUint));
+		memset(&m_sUnitExArr[i], 0, sizeof(CUUintEx));
 	}
 
 	m_pTexCurrent = m_pTexStates[CERASUS_STATE_NORMAL];
@@ -48,17 +50,41 @@ CCerasusBlendTex::~CCerasusBlendTex()
 }
 
 //------------------------------------------------------------------
+// @Function:	 SetUnit()
+// @Purpose: CCerasusBlendTex设置纹理参数
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+void CCerasusBlendTex::SetUnit(UINT nState, CUUint sUnit)
+{
+	memcpy_s(&m_sUnitArr[nState], sizeof(CUUint), &sUnit, sizeof(CUUint));
+}
+
+//------------------------------------------------------------------
+// @Function:	 SetUnitEx()
+// @Purpose: CCerasusBlendTex设置纹理参数(Ex)
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+void CCerasusBlendTex::SetUnitEx(UINT nState, CUUintEx sUnit)
+{
+	memcpy_s(&m_sUnitExArr[nState], sizeof(CUUintEx), &sUnit, sizeof(CUUintEx));
+}
+
+//------------------------------------------------------------------
 // @Function:	 Init()
 // @Purpose: CCerasusBlendTex初始化控件纹理
 // @Since: v1.00a
 // @Para: None
 // @Return: None
 //------------------------------------------------------------------
-void CCerasusBlendTex::Init(CUUint sUnitArr[MAX_CONTROL_STATES])
+void CCerasusBlendTex::Init()
 {
 	for (int i = 0; i < MAX_CONTROL_STATES; ++i)
 	{
-		m_pTexStates[i]->CCerasusUnitInit(sUnitArr[i]);
+		m_pTexStates[i]->CCerasusUnitInit(m_sUnitArr[i]);
 	}
 
 }
@@ -70,11 +96,11 @@ void CCerasusBlendTex::Init(CUUint sUnitArr[MAX_CONTROL_STATES])
 // @Para: None
 // @Return: None
 //------------------------------------------------------------------
-void CCerasusBlendTex::Init(CUUintEx sUnitArr[MAX_CONTROL_STATES])
+void CCerasusBlendTex::InitEx()
 {
 	for (int i = 0; i < MAX_CONTROL_STATES; ++i)
 	{
-		m_pTexStates[i]->CCerasusUnitInit(sUnitArr[i]);
+		m_pTexStates[i]->CCerasusUnitInit(m_sUnitExArr[i]);
 	}
 
 }
@@ -96,4 +122,102 @@ void CCerasusBlendTex::Blend(UINT nState)
 	m_pTexCurrent->CCerasusUnitSetRenderState();			// 设置渲染状态
 	m_pTexCurrent->CCerasusUnitRender();					// 渲染图形
 	m_pTexCurrent->CCerasusUnitSetAlphaBlendDisable();		// 关闭Alpha缓冲
+}
+
+//------------------------------------------------------------------
+// @Function:	 CCerasusBlendFont()
+// @Purpose: CCerasusBlendFont构造函数
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+CCerasusBlendFont::CCerasusBlendFont(IDirect3DDevice9* pD3D9Device)
+{
+	for (int i = 0; i < MAX_CONTROL_STATES; ++i)
+	{
+		m_pFontStates[i] = new DirectFont(pD3D9Device);
+	}
+
+	m_nFontSize = 12;
+	m_lpszFontType = (LPWSTR)_T("Consolas");
+	m_hWnd = NULL;
+	m_lpcszStr = _T("");
+	m_dwFormat = DT_CENTER | DT_VCENTER;
+	m_dwColor = D3DCOLOR_ARGB(255, 255, 255, 255);
+
+	m_pFontCurrent = m_pFontStates[CERASUS_STATE_NORMAL];
+}
+
+//------------------------------------------------------------------
+// @Function:	 ~CCerasusBlendFont()
+// @Purpose: CCerasusBlendFont析构函数
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+CCerasusBlendFont::~CCerasusBlendFont()
+{
+	for (int i = 0; i < MAX_CONTROL_STATES; ++i)
+	{
+		SAFE_DELETE(m_pFontStates[i]);
+	}
+
+	m_pFontCurrent = NULL;
+}
+
+//------------------------------------------------------------------
+// @Function:	 SetFontProperty()
+// @Purpose: CCerasusBlendFont设置字体属性
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+void CCerasusBlendFont::SetFontProperty(int nFontSize, LPWSTR lpszFontType)
+{
+	m_nFontSize = nFontSize;
+	m_lpszFontType = lpszFontType;
+}
+
+//------------------------------------------------------------------
+// @Function:	 SetFontText()
+// @Purpose: CCerasusBlendFont设置字体内容
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+void CCerasusBlendFont::SetFontText(HWND hWnd, LPCWSTR lpcszStr, DWORD dwFormat, D3DCOLOR dwColor)
+{
+	m_hWnd = hWnd;
+	m_lpcszStr = lpcszStr;
+	m_dwFormat = dwFormat;
+	m_dwColor = dwColor;
+}
+
+//------------------------------------------------------------------
+// @Function:	 Init()
+// @Purpose: CCerasusBlendFont初始化控件字体
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+void CCerasusBlendFont::Init()
+{
+	for (int i = 0; i < MAX_CONTROL_STATES; ++i)
+	{
+		m_pFontStates[i]->DirectFontInit(m_nFontSize, m_lpszFontType);
+	}
+
+}
+
+//------------------------------------------------------------------
+// @Function:	 Blend()
+// @Purpose: CCerasusBlendFont渲染控件字体
+// @Since: v1.00a
+// @Para: None
+// @Return: None
+//------------------------------------------------------------------
+void CCerasusBlendFont::Blend(UINT nState)
+{
+	m_pFontCurrent = m_pFontStates[nState];
+	m_pFontCurrent->DirectFontDrawText(m_hWnd, m_lpcszStr, m_dwFormat, m_dwColor);
 }
